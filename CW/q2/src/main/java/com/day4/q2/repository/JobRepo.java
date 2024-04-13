f18606095b09ef7788b920dae52e02ae9c1a46f3
VEDHAPRAKASH2 <165143988+VEDHAPRAKASH2@users.noreply.github.com>
2024-04-13 14:46:26 +0530

    Delete CW directory

--- CW/q2/src/main/java/com/day4/q2/repository/JobRepo.java
+++
@@ -1,9 +0,0 @@
-package com.day4.q2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.day4.q2.model.Job;
-
-public interface JobRepo  extends JpaRepository<Job,Integer>{
-    
-}
