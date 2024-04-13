306012ffef374e11ecc1207785aa36978b659417
VEDHAPRAKASH2 <165143988+VEDHAPRAKASH2@users.noreply.github.com>
2024-04-13 14:42:50 +0530

    Delete Day 4 directory

--- Day 4/PAH/q1/src/main/java/com/day4/q1/repository/BookRepo.java
+++
@@ -1,8 +0,0 @@
-package com.day4.q1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.day4.q1.model.Book;
-
-public interface BookRepo extends JpaRepository<Book,Integer>{   
-}
